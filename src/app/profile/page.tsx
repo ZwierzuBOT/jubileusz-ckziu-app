2b856f7630fe17464af4b34b18551afa2c5b786b
Mateusz Zwierzchowski <zwierzchowski.mateo@gmail.com>
2024-10-23 20:52:40 +0200

    commit

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,7 @@
 const page = () => {
     return ( 
         <div>
-            <h1 className="text-black">profile</h1>
+            <h1 className="text-black">Profile</h1>
         </div>
      );
 }
--- src/app/regulamin/page.tsx
+++ src/app/regulamin/page.tsx
@@ -1,7 +1,7 @@
 const page = () => {
     return ( 
         <div>
-            <h1 className="text-black">Reuglamin</h1>
+            <h1 className="text-black">Regulamin</h1>
         </div>
      );
 }
